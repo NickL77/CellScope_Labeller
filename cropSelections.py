45eb1c614b65aead4b800a4b9a6660314dfdff6e
Nicholas Liu <nicholas.j.liu@gmail.com>
2018-12-02 14:22:25 -0800

    Added cropping for all 3 image types

--- cropSelections.py
+++ cropSelections.py
@@ -11,17 +11,21 @@ for filename in os.listdir(label_dir):
 	if filename.endswith('.txt'):
 
 		f = open(os.path.join(label_dir, filename), 'r')
-		img_name = f.readline().rstrip()
+		img_name_0 = f.readline().rstrip()
+		img_name_1 = f.readline().rstrip()
+		img_name_2 = f.readline().rstrip()
+		imgs = [img_name_0, img_name_1, img_name_2]
 		b_boxes = ast.literal_eval(f.readline().rstrip())
 
 		for i in range(len(b_boxes) // 2):
 			bl_x, bl_y = b_boxes[i * 2][0], b_boxes[i * 2][1]
 			tr_x, tr_y = b_boxes[i * 2 + 1][0], b_boxes[i * 2 + 1][1]
-			
-			img = cv2.imread(img_name)
-			crop_img = img[bl_y: tr_y, bl_x: tr_x]
-			crop_img_name = img_name + '_' + str(i) + '.png'
-			cv2.imwrite(os.path.join(cropped_dir, crop_img_name), crop_img)
+		
+			for j in range(len(imgs)):
+				img = cv2.imread(dirname + '/' + imgs[j])
+				crop_img = img[bl_y: tr_y, bl_x: tr_x]
+				crop_img_name = imgs[j][15:len(imgs[j]) - 4] + '_crop' + str(i) + '.png'
+				cv2.imwrite(os.path.join(cropped_dir, crop_img_name), crop_img)
 
 		f.close()
 
