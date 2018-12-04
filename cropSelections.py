243846837fcba358e175676d59d8e19f5e304f4c
Nicholas Liu <nicholas.j.liu@gmail.com>
2018-12-04 01:55:50 -0800

    Added cropping negatives

--- cropSelections.py
+++ cropSelections.py
@@ -16,6 +16,7 @@ for filename in os.listdir(label_dir):
 		img_name_2 = f.readline().rstrip()
 		imgs = [img_name_0, img_name_1, img_name_2]
 		b_boxes = ast.literal_eval(f.readline().rstrip())
+		neg_boxes = ast.literal_eval(f.readline().rstrip())
 
 		for i in range(len(b_boxes) // 2):
 			bl_x, bl_y = b_boxes[i * 2][0], b_boxes[i * 2][1]
@@ -26,6 +27,17 @@ for filename in os.listdir(label_dir):
 				crop_img = img[bl_y: tr_y, bl_x: tr_x]
 				crop_img_name = imgs[j][15:len(imgs[j]) - 4] + '_crop' + str(i) + '.png'
 				cv2.imwrite(os.path.join(cropped_dir, crop_img_name), crop_img)
+
+		for i in range(len(neg_boxes) // 2):
+			bl_x, bl_y = neg_boxes[i * 2][0], neg_boxes[i * 2][1]
+			tr_x, tr_y = neg_boxes[i * 2 + 1][0], neg_boxes[i * 2 + 1][1]
+		
+			for j in range(len(imgs)):
+				img = cv2.imread(dirname + '/' + imgs[j])
+				crop_img = img[bl_y: tr_y, bl_x: tr_x]
+				crop_img_name = imgs[j][15:len(imgs[j]) - 4] + '_neg' + str(i) + '.png'
+				cv2.imwrite(os.path.join(cropped_dir, crop_img_name), crop_img)
+
 
 		f.close()
 
